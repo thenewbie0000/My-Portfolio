8767aba1bca55d6b7aea6619b4e88ab37154561c
Rojesh Shrestha <shrestharojesh34@gmail.com>
2024-04-12 10:45:21 +0545

    Updated contact form

--- script.js
+++ script.js
@@ -21,39 +21,46 @@ function closeMenu(){
   sidemenu.style.right = "-50%";
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycby_ZD_M65f5pvYnSdZqZuXPBHfv6viAv5J7pWU84_gNwFXPw9jbswUAbzQvGQ72ruYfug/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzZ7LXgu69GZa1uxQ_FGwDl9p6UbTg8hLXsaL6P3Omt8wHMLXpF6SOZIzUeRkUt2kVz4w/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
+  e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      msg.innerHTML = "Message Sent Successfully!"
-      form.reset();
-      setTimeout(()=>{msg.innerHTML = ''}, 5000);
-    })
-    .catch(error => console.error('Error!', error.message))
+  .then(response => {
+    if (response.ok) {
+      return response.text();
+    } else {
+      throw new Error('Network response was not ok.');
+    }
+  })
+  .then(data => {
+    msg.innerHTML = "Message Sent Successfully!";
+    form.reset(); 
+    setTimeout(() => {
+      msg.innerHTML = ''; 
+    }, 5000);
+  })
+  .catch(error => {
+    console.error('Error!', error.message);
+    msg.innerHTML = "Error sending message. Please try again later.";
+  });
 })
 
 const seeMoreBtn = document.querySelector('.see-more-btn');
-const hiddenWorks = document.querySelectorAll('.work:nth-child(n+4)'); // Selects the works after the fourth one
-
-// Initially show the first four works
+const hiddenWorks = document.querySelectorAll('.work:nth-child(n+4)'); 
 const initialWorks = document.querySelectorAll('.work:nth-child(-n+3)');
 initialWorks.forEach(work => {
     work.classList.add('show');
 });
 
 seeMoreBtn.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior of the link
-
-    // Toggle a class to show/hide the additional works
+    event.preventDefault(); 
     hiddenWorks.forEach(work => {
         work.classList.toggle('show');
     });
 
-    // Change the text of the button based on the state
     if (seeMoreBtn.textContent === 'See More') {
         seeMoreBtn.textContent = 'See Less';
     } else {
